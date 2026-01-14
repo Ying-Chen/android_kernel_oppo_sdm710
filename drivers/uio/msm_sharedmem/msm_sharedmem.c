7e0181abfa1c8a2be3af5cf2fcd9e904884f7cf9
Andrey Sprynga <rtx4d@aospa.co>
2026-01-14 16:55:15 +0800

    uio: Import oembackup_sharedmem changes
    Change-Id: Ic62861d11705c784171372b88b39c0cc7f559a94
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- drivers/uio/msm_sharedmem/msm_sharedmem.c
+++ drivers/uio/msm_sharedmem/msm_sharedmem.c
@@ -28,6 +28,9 @@
 
 #define MPSS_RMTS_CLIENT_ID 1
 
+// Zhengpeng.Tan@NW.MDM.NV.892767, 2016/11/30
+#define MPSS_OEMBACK_CLIENT_ID 4
+
 static int uio_get_mem_index(struct uio_info *info, struct vm_area_struct *vma)
 {
 	if (vma->vm_pgoff >= MAX_UIO_MAPS)
@@ -86,8 +89,10 @@ static void setup_shared_ram_perms(u32 client_id, phys_addr_t addr, u32 size)
 	int dest_perms[2] = {PERM_READ|PERM_WRITE,
 			     PERM_READ|PERM_WRITE};
 
-	if (client_id != MPSS_RMTS_CLIENT_ID)
-		return;
+	if (client_id != MPSS_RMTS_CLIENT_ID
+    // Zhengpeng.Tan@NW.MDM.NV.892767, 2016/11/30
+    // add for nv backup and restore
+		&&client_id != MPSS_OEMBACK_CLIENT_ID)
 
 	ret = hyp_assign_phys(addr, size, source_vmlist, 1, dest_vmids,
 				dest_perms, 2);
